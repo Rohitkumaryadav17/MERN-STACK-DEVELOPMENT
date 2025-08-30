d8828412a55661ebf574ff0a077ee0029275a1c2
Rohit Kumar Yadav <rky5825@gmail.com>
2025-08-30 13:49:03 +0530

    mernstack

--- My first project/src/Product.jsx
+++ My first project/src/Product.jsx
@@ -1,10 +1,11 @@
+import "./Product.css";
 function Product(){
     return(
-        <div>
+        <div className="Product">
             <h1>Jira softawre</h1>
-            <h2>Product discription</h2>
+            <h2>Product discription</h2> 
         </div>
-    )
+    );
 }
 export default Product;
 

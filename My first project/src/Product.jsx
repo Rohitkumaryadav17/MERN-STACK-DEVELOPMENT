d9d7e1414b7b12eb1f2ac1ab997e1cb21443dcbd
Rohit Kumar Yadav <rky5825@gmail.com>
2025-08-30 19:59:17 +0530

    mernstack

--- My first project/src/Product.jsx
+++ My first project/src/Product.jsx
@@ -1,11 +1,22 @@
 import "./Product.css";
-function Product(){
-    return(
-        <div className="Product">
-            <h1>Jira softawre</h1>
-            <h2>Product discription</h2> 
-        </div>
-    );
-}
-export default Product;
+function Product({ title,price,features2}){
+ const list=features2.map((feature) => <li>{feature}</li>);
+//  let isDiscount=price>30000 ?"discount of 5% ":" ";
+let styles={backgroundColor:price>30000 ? "pink":" "};
+ 
 
+  
+    return(
+        <div className="Product" style={styles}>
+            <h1>{title}</h1>
+            <h2>price:{price}</h2> 
+        
+           <p>{list}</p> 
+          {price>30000 ?<p>discount of 5%</p>:<a>get dicount</a>}
+
+        </div>   
+    ); 
+}   
+export default Product;  
+ 
+       
--- My first project/src/Producttab.jsx
+++ My first project/src/Producttab.jsx
@@ -1,14 +1,19 @@
 import Product from "./Product.jsx";
 function Producttab(){
+    let option=["hi-tech", "durable","fast"];
+  
+
+    // let option2={a: "Hi-tech",b:"durable",c:"fast"};
 
     return(
-        <>
-  <Product/>
-  <Product/>  
-    
-  <Product/>
-
-        </>
+    <>
+  <Product title="Phone" price={2000} features2={option}/>
+    <Product title="Laptop" price={35000} features2={option}/>
+      <Product title="Earbud" price={40000} features2={option}/>
+ 
+ 
+     
+        </> 
 
     );
 }

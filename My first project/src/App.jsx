e8724ad211a5a5188047a4fa3bb63b189a5795ca
Rohit Kumar Yadav <rky5825@gmail.com>
2025-08-30 10:25:51 +0530

    mernstack

---
+++ My first project/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css"
+
+function Title(){
+  return <h1>rohit kumar</h1>;
+}
+function App() {
+  return (
+  <div>
+  <p>inside app component we have paragraph and heading tag</p>j
+  <h1>this is my app component </h1>
+  
+  <Title/>
+
+  </div>
+  );
+
+}
+export default App ;
+    
